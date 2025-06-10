62d1639efec328a541802671745ba79f89ded6e8
opalissan <opal.issan@gmail.com>
2025-06-09 20:25:48 -0700

    add mixed methods #1

--- operators/mixed_method_1/mixed_method_1_operators.py
+++ operators/mixed_method_1/mixed_method_1_operators.py
@@ -3,3 +3,25 @@
 Author: Opal Issan (oissan@ucsd.edu)
 Last Update: June 9th, 2025
 """
+import numpy as np
+
+
+def extra_term_1(n, LH_int, v_b, v_a, C_hermite_last, alpha, Nv_H, D, E):
+    """
+
+    :param n:
+    :param LH_int:
+    :param v_b:
+    :param v_a:
+    :param C_hermite_last:
+    :param alpha:
+    :param Nv_H:
+    :param D:
+    :param E:
+    :return:
+    """
+    A = alpha * np.sqrt(Nv_H / 2) * (D @ C_hermite_last - 2 / (alpha ** 2) * E * C_hermite_last)
+    return - A / (v_b - v_a) * LH_int[n]
+
+
+
--- operators/mixed_method_1/setup_mixed_method_1_two_stream.py
+++ operators/mixed_method_1/setup_mixed_method_1_two_stream.py
@@ -5,3 +5,85 @@ ions are treated as stationary
 Author: Opal Issan (oissan@ucsd.edu)
 Last Update: June 9th, 2025
 """
+import numpy as np
+from operators.legendre.legendre_operators import A1_legendre, sigma_bar, B_legendre, xi_legendre
+from operators.hermite.hermite_operators import A1_hermite, psi_hermite
+from operators.universal_functions import get_D_inv, A2, A3
+from operators.finite_difference import ddx_central
+import scipy
+
+
+class SimulationSetupMixedMethod0:
+    def __init__(self, Nx, Nv_H, Nv_L, epsilon, v_a, v_b, alpha, u, gamma, L, dt, T0, T, nu_H, nu_L, Nv_int=1000,
+                 m_e=1, m_i=1836, q_e=-1, q_i=1, problem_dir=None):
+        # velocity grid
+        # set up configuration parameters
+        # spatial resolution
+        self.Nx = Nx
+        # velocity resolution
+        self.Nv_H = Nv_H
+        self.Nv_L = Nv_L
+        # epsilon displacement in initial electron distribution
+        self.epsilon = epsilon
+        # velocity boundaries
+        self.v_a = v_a
+        self.v_b = v_b
+        # hermite scaling and shifting parameters
+        self.alpha = alpha
+        self.u = u
+        # penalty magnitude
+        self.gamma = gamma
+        # x grid is from 0 to L
+        self.L = L
+        self.dx = self.L / self.Nx
+        # time stepping delta t
+        self.dt = dt
+        # final time
+        self.T = T
+        # initial start
+        self.T0 = T0
+        # vector of timestamps
+        self.t_vec = np.linspace(self.T0, self.T, int((self.T - self.T0) / self.dt) + 1)
+        # mass normalized
+        self.m_e = m_e
+        self.m_i = m_i
+        # charge normalized
+        self.q_e = q_e
+        self.q_i = q_i
+        # artificial collisional frequency
+        self.nu_H = nu_H
+        self.nu_L = nu_L
+        # directory name
+        self.problem_dir = problem_dir
+
+        # matrices
+        # finite difference derivative matrix
+        self.D = ddx_central(Nx=self.Nx + 1, dx=self.dx, periodic=True, order=2)
+        self.D_inv = get_D_inv(Nx=self.Nx, D=self.D)
+
+        # Hermite operator
+        self.A_e_H = self.alpha * A1_hermite(D=self.D, Nv=self.Nv_H) \
+                     + self.u * A2(D=self.D, Nv=self.Nv_H) \
+                     + self.nu_H * A3(Nx=self.Nx, Nv=self.Nv_H)
+
+        # Legendre operators
+        self.A_e_L = A1_legendre(D=self.D, Nv=self.Nv_L, v_a=v_a, v_b=v_b) \
+                     + sigma_bar(v_a=self.v_a, v_b=self.v_b) * A2(D=self.D, Nv=self.Nv_L) \
+                     + self.nu_L * A3(Nx=self.Nx, Nv=self.Nv_L)
+
+        self.B_e_L = B_legendre(Nv=self.Nv_L, Nx=self.Nx, v_a=self.v_a, v_b=self.v_b)
+
+        # xi functions
+        self.xi_v_a = np.zeros(self.Nv_L)
+        self.xi_v_b = np.zeros(self.Nv_L)
+        for nn in range(self.Nv_L):
+            self.xi_v_a[nn] = xi_legendre(n=nn, v=self.v_a, v_a=self.v_a, v_b=self.v_b)
+            self.xi_v_b[nn] = xi_legendre(n=nn, v=self.v_b, v_a=self.v_a, v_b=self.v_b)
+
+        v_ = np.linspace(v_a, v_b, Nv_int, endpoint=True)
+        self.LH_int = np.zeros(self.Nv_L)
+        for nn in range(self.Nv_L):
+            self.LH_int[nn] = scipy.integrate.trapezoid(xi_legendre(n=nn, v=v_, v_a=self.v_a, v_b=self.v_b)
+                                                        * psi_hermite(n=self.Nv_H, alpha_s=self.alpha, u_s=self.u,
+                                                                      v=v_),
+                                                        x=v_, dx=np.abs(v_[1] - v_[0]))
