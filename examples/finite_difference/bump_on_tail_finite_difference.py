37b9720407fd09e9463a4811fa615261bd9de440
opalissan <opal.issan@gmail.com>
2025-11-26 17:16:28 -0800

    conservation laws

---
+++ examples/finite_difference/bump_on_tail_finite_difference.py
@@ -0,0 +1,5 @@
+"""Module to simulate the bump on tail instability with 1D1V finite difference code
+
+Last modified: Nov 26th, 2025
+Author: Opal Issan (oissan@ucsd.edu)
+"""
---
+++ examples/finite_difference/two_stream_finite_difference.py
@@ -0,0 +1,5 @@
+"""Module to simulate the two stream instability with 1D1V finite difference code
+
+Last modified: Nov 26th, 2025
+Author: Opal Issan (oissan@ucsd.edu)
+"""
